c3e20335b3a4bbd4bd3148165370a8fa86d8fa63
JGabrielFDC <jgabrielfdc08@gmail.com>
2024-08-05 07:22:27 -0300

    Atualização

--- components/header_links.php
+++ components/header_links.php
@@ -1,9 +1,2 @@
 <link href="../components/bootstrap4/css/bootstrap.min.css" rel="stylesheet"> 
-<link href="../../JQM_Cars//public/style.css" rel="stylesheet">
-
-<?php 
-function timer($tempo,$valor){
-    sleep($tempo); 
-    unset($valor);
-}
-?>
+<link href="../public/style.css" rel="stylesheet">
